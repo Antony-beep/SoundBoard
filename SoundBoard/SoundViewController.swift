63e9e095393babfc01ebde1741dbe29e165a6069
Lourdes <lourdes.pampa@tecsup.edu.pe>
2020-05-14 01:04:08 -0500

    "Mostar lista de grabaciones"

--- SoundBoard/SoundViewController.swift
+++ SoundBoard/SoundViewController.swift
@@ -84,7 +84,7 @@ class SoundViewController: UIViewController {
     }
     /*
     // MARK: - Navigation
-
+			
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
